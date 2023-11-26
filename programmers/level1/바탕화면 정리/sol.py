eed9c237e5a7f0251603235eb30317f24dbcf7e1
hhjjoa2817 <hhjjoa2817@naver.com>
2023-11-27 07:50:20 +0900

    23.11.27

--- programmers/level1/바탕화면 정리/sol.py
+++ programmers/level1/바탕화면 정리/sol.py
@@ -1,3 +1,21 @@
 def solution(wallpaper):
-    answer = []
+    answer = [0, 0, 0, 0]
+    stack = [] # '#'이 있는 행
+    stack1 = [] # '#'이 가장 처음에 있는 열
+    stack2 = [] # '#'이 가장 마지막에 있는 열
+    
+    for i in range(len(wallpaper)):
+        if '#' in wallpaper[i]:
+            stack.append(i)
+            stack1.append(wallpaper[i].find('#'))
+            stack2.append(wallpaper[i].rfind('#'))
+
+    answer[0] = min(stack)
+    answer[2] = max(stack) + 1
+    answer[1] = min(stack1)
+    answer[3] = max(stack2) + 1
+
     return answer
+
+
+print(solution([".#...", "..#..", "...#."]))
