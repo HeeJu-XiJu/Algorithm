0758731f3bba0d10f64adf2b4db0e1c5f7ec8f9d
hhjjoa2817 <hhjjoa2817@naver.com>
2023-11-30 08:16:25 +0900

    23.11.30

--- programmers/level1/데이터분석/sol.py
+++ programmers/level1/데이터분석/sol.py
@@ -18,3 +18,16 @@ def solution(data, ext, val_ext, sort_by):
 
 
 print(solution([[1, 20300104, 100, 80], [2, 20300804, 847, 37], [3, 20300401, 10, 8]], 'date', 20300501, "remain"))
+
+
+
+# 다른 풀이
+# def solution(data, ext, v_ext, srt):
+#     answer = []
+#     mdict = {"code":0, "date":1, "maximum":2, "remain":3}
+    
+#     for d in data:
+#         if d[mdict[ext]] < v_ext:
+#             answer.append(d)
+
+#     return sorted(answer, key = lambda x: x[mdict[srt]])
