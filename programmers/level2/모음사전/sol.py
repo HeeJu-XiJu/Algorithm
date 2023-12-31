261403860d420ef180316dc74a91766d0b56167d
hhjjoa2817 <hhjjoa2817@naver.com>
2023-12-31 10:19:04 +0900

    23.12.31

--- programmers/level2/모음사전/sol.py
+++ programmers/level2/모음사전/sol.py
@@ -1,3 +1,23 @@
+# from itertools import permutations
+# def solution(word):
+#     words = ['A', 'E', 'I', 'O', 'U']
+    
+#     searchlist = set(words)
+#     searchwords = words * 5
+#     for num in range(2, 6):
+#         permutation = permutations(searchwords, num)
+#         permutation = set((permutation))
+#         for str in permutation:
+#             searchlist.add(''.join(str))
+#     answer = sorted(searchlist).index(word) + 1
+    
+#     return answer
+
+# print(solution('AAAAE'))
+
+
+# 이진탐색
+
 from itertools import permutations
 def solution(word):
     words = ['A', 'E', 'I', 'O', 'U']
@@ -6,11 +26,20 @@ def solution(word):
     searchwords = words * 5
     for num in range(2, 6):
         permutation = permutations(searchwords, num)
-        searchlist.add(list(permutation))
-    print(searchlist)
-    # print(sorted(['A', 'AA', 'AE', 'EA', 'E']))
-    answer = 0
-    
-    return answer
+        permutation = set((permutation))
+        for str in permutation:
+            searchlist.add(''.join(str))
+
+    searchlist = sorted(searchlist)
+    left = 0
+    right = len(searchlist)
+    while left <= right:
+        middle = (left + right) // 2
+        if word == searchlist[middle]:
+            return middle + 1
+        elif word < searchlist[middle]:
+            right = middle - 1
+        else :
+            left = middle + 1
 
 print(solution('AAAAE'))
