259f00039fa9208d41b6bcdd3bce8ab7beec5a3d
hhjjoa2817 <hhjjoa2817@naver.com>
2024-01-05 08:40:07 +0900

    24.01.05

--- programmers/level2/모음사전/sol.py
+++ programmers/level2/모음사전/sol.py
@@ -43,3 +43,15 @@ def solution(word):
             left = middle + 1
 
 print(solution('AAAAE'))
+
+
+## 다른 풀이
+from itertools import product
+
+def solution(word):
+    myL = []
+    for i in range(1, 6):
+        for k in product('AEIOU', repeat=i): 
+            myL.append(''.join(list(k))) 
+
+    return sorted(myL).index(word) + 1
