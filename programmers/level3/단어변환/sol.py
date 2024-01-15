59ee24eaf4a0c393f4777f75c2da7da193dc060c
hhjjoa2817 <hhjjoa2817@naver.com>
2024-01-15 09:23:35 +0900

    24.01.15

--- programmers/level3/단어변환/sol.py
+++ programmers/level3/단어변환/sol.py
@@ -1,5 +1,22 @@
+from collections import deque
+
 def solution(begin, target, words):
-    answer = 0
-    return answer
+    queue = deque()
+    queue.append([begin, 0])
+    visited = []
+    if target not in words:
+        return 0
+    while queue:
+        word, count = queue.popleft()
+        if word == target:
+            return count
+        for w in words:
+            diff = 0
+            for idx in range(len(word)):
+                if word[idx] != w[idx]:
+                    diff += 1
+            if diff == 1:
+                visited.append(w)
+                queue.append([w, count+1])
 
 print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
