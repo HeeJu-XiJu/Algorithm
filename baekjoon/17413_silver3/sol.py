64077e7082f221cbe26e338756b108d77214352c
hhjjoa2817 <hhjjoa2817@naver.com>
2024-04-01 01:54:50 +0900

    24.04.01

--- baekjoon/17413_silver3/sol.py
+++ baekjoon/17413_silver3/sol.py
@@ -1,31 +1,34 @@
 import sys
-from collections import deque
 sys.stdin = open('input.txt')
 
-string = sys.stdin.readline()
+string = input()
 special = 0
 answer = ''
-voca = deque()
+voca = []
 
 for s in string:
     if s == '<':
+        number = len(voca)
+        for _ in range(number):
+            answer += voca.pop()
         special = 1
-        voca.append(s)
+        answer += s
     elif s == '>':
         special = 0
-        voca.append(s)
-        answer += ''.join(voca)
-        voca = deque()
-    elif s == ' ':
-        answer += ''.join(voca)
-        answer += ' '
-        voca = deque()
+        answer += s
     elif special == 1:
-        voca.append(s)
+        answer += s
+    elif s == ' ' and special == 0:
+        number = len(voca)
+        for _ in range(number):
+            answer += voca.pop()
+        answer += ' '
     else:
-        voca.appendleft(s)
+        voca.append(s)
 
-answer += ''.join(voca)
-answer.strip()
+number = len(voca)
+if number != 0:
+    for _ in range(number):
+        answer += voca.pop()
 
 print(answer)
