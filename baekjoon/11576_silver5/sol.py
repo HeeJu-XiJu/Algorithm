c0d3b0d66cd43e53d591f8c4f231ee1d3dc74640
hhjjoa2817 <hhjjoa2817@naver.com>
2024-04-30 17:36:51 +0900

    24.04.28 review

--- baekjoon/11576_silver5/sol.py
+++ baekjoon/11576_silver5/sol.py
@@ -1,2 +1,20 @@
 import sys
 sys.stdin = open('input.txt')
+
+A, B = map(int, input().split())
+n = int(input())
+mlist = list(map(int, input().split()))
+mlist.reverse()
+num_10 = 0
+
+for i in range(n):
+    num_10 += A ** i * mlist[i]
+
+answerlist = []
+while num_10 >= 1:
+    a = num_10 % B
+    num_10 = num_10 // B
+
+    answerlist.append(a)
+
+print(' '.join(map(str, answerlist[::-1])))
