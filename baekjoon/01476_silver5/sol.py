ee9970a0feb21770ca9a07701ed9abb14ce3eb42
hhjjoa2817 <hhjjoa2817@naver.com>
2024-05-24 15:14:55 +0900

    24.05.22

---
+++ baekjoon/01476_silver5/sol.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('input.txt')
+
+E, S, M = map(int, input().split(' '))
+
+answer = 1
+
+while True:
+    if ((answer-E) % 15 == 0) and ((answer-S) % 28 == 0) and ((answer-M) % 19 == 0):
+        break
+    answer += 1
+
+print(answer)
